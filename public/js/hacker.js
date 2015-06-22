b15770ff2b36bc13ae6d02809392e39878179a2c
AnonSatan <anonsatan@outlook.com>
2015-06-21 21:19:35 -0700

    Moving to angular step by step...

--- public/js/hacker.js
+++ public/js/hacker.js
@@ -1,1 +1,13 @@
-// JavaScript source code
+(function () {
+    "use strict";
+    var hthApp = angular.module("hthApp", ['ui.router']);
+
+    hthApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
+    function ($stateProvider, $urlRouterProvider, $locationProvider){
+        $stateProvider
+            .state("home", {url: "/", templateUrl: "./templates/home.html" });
+        $urlRouterProvider.otherwise("/");
+        $locationProvider.html5Mode(true);
+    }])
+
+}());
