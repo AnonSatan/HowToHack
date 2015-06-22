67369b0db76b9863083eef033fe1666c23d7de6c
AnonSatan <anonsatan@outlook.com>
2015-06-21 21:48:32 -0700

    Moving to angular step by step...

--- public/js/hacker.js
+++ public/js/hacker.js
@@ -3,11 +3,15 @@
     var hthApp = angular.module("hthApp", ['ui.router']);
 
     hthApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
-    function ($stateProvider, $urlRouterProvider, $locationProvider){
-        $stateProvider
-            .state("home", {url: "/", templateUrl: "./templates/home.html" });
-        $urlRouterProvider.otherwise("/");
-        $locationProvider.html5Mode(true);
-    }])
+        function ($stateProvider, $urlRouterProvider, $locationProvider) {
+            $stateProvider
+                .state("home", {url: "/", templateUrl: "./templates/home.html"})
+                .state("one", {url: "/one", templateUrl: "./templates/one.html"})
+                .state("two", {url: "/two", templateUrl: "./templates/two.html"})
+                .state("three", {url: "/three", templateUrl: "./templates/three.html"})
+                .state("four", {url: "/four", templateUrl: "./templates/four.html"});
+            $urlRouterProvider.otherwise("/");
+            $locationProvider.html5Mode(true);
+        }])
 
 }());
---
+++ public/js/hthNav.js
@@ -0,0 +1,18 @@
+/**
+ *  Created by David Prentice on 6/21/2015.
+ *  All Rights Reserved, 2015.
+ */
+/**
+ *  Created by David Prentice on 6/20/2015.
+ *  All Rights Reserved, 2015.
+ */
+(function () {
+    "use strict";
+    angular.module('hthApp')
+        .directive('hthNav', function () {
+            return {
+                restrict: 'E',
+                templateUrl: "./templates/hthNav.html"
+            }
+        })
+}());
