0b2ebc8bdf10aec975d058d92317724fb0491f3f
AnonSatan <anonsatan@outlook.com>
2015-06-28 02:58:00 -0700

    Refactored home.html to hthNav.html. Style changes.

---
+++ public/js/hthTabs.js
@@ -0,0 +1,10 @@
+(function () {
+    "use strict";
+    angular.module('hthApp')
+        .directive('hthTabs', function () {
+            return {
+                restrict: 'E',
+                templateUrl: "./templates/hthTabs.html"
+            }
+        })
+}());
