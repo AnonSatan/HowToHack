458460ab506525dffd8b7a4e3d7e1311f24ea157
AnonSatan <anonsatan@outlook.com>
2015-12-27 00:51:26 -0800

    Code cleanup

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
-var express = require('express');
-
-var app = express();
-
-app.set('view options', {
-    layout: false
-});
-
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/node_modules'));
-app.use(express.static(__dirname + '/templates'));
-
-app.get('/', function (req, res) {
+(function () {
     "use strict";
-    res.render('index.html');
-});
 
-app.get('*', function (req, res) {
-    "use strict";
-    res.redirect('/');
-});
+    var express = require('express'),
+        app = express();
+    app.use(express.static('public'));
+    app.use(express.static('node_modules'));
+    app.use(express.static('templates'));
 
-app.listen(3000, function () {
-    "use strict";
-    console.log('Express server listening on port 3000...');
-});
+    app.get('*', function (req, res) {
+        res.redirect('/');
+    });
+
+    app.listen(3000, function () {
+        console.log('Express server listening on port 3000...');
+    });
+}());
--- public/js/hacker.js
+++ public/js/hacker.js
@@ -1,8 +1,6 @@
 (function () {
     "use strict";
-
     var hthApp = angular.module("hthApp", ['ui.router']);
-
     hthApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
         function ($stateProvider, $urlRouterProvider, $locationProvider) {
             $stateProvider
