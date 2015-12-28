2c6f2940c8d3201aca7be7b83266ae607ae4cdf7
AnonSatan <anonsatan@outlook.com>
2015-12-28 05:01:13 -0800

    Express to angular routing fixed.

--- app.js
+++ app.js
@@ -7,11 +7,13 @@
     app.use(express.static('node_modules'));
     app.use(express.static('templates'));
 
-    app.get('*', function (req, res) {
-        res.redirect('/');
+    app.all('/*', function (req, res) {
+        console.log(req.path);
+        res.sendFile('index.html', {root: __dirname});
     });
 
     app.listen(3000, function () {
         console.log('Express server listening on port 3000...');
     });
 }());
+//comment
