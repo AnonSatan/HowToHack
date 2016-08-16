eb0964f78683917a3fd4b97499e8c79e15acd227
AnonSatan <anonsatan@outlook.com>
2016-08-16 14:19:53 -0700

    Refactoring...

---
+++ public/js/ngApp.js
@@ -0,0 +1,5 @@
+(function () {
+    "use strict";
+    var hthApp = angular.module("hthApp", []);
+    hthApp();
+}());
---
+++ public/js/routes.js
@@ -0,0 +1,54 @@
+(function () {
+    "use strict";
+    angular.module("hthApp", ['ui.router'])
+        .config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
+            function ($stateProvider, $urlRouterProvider, $locationProvider) {
+                $stateProvider
+                    .state("home", {
+                        url: "/",
+                        templateUrl: "./templates/home.html"
+                    })
+                    .state("one", {
+                        url: "/one",
+                        templateUrl: "./templates/one.html"
+                    })
+                    .state("two", {
+                        url: "/two",
+                        templateUrl: "./templates/two.html"
+                    })
+                    .state("three", {
+                        url: "/three",
+                        templateUrl: "./templates/three.html"
+                    })
+                    .state("four", {
+                        url: "/four",
+                        templateUrl: "./templates/four.html"
+                    })
+                    .state("five", {
+                        url: "/five",
+                        templateUrl: "./templates/five.html"
+                    })
+                    .state("signUp", {
+                        url: "/signUp",
+                        templateUrl: "./templates/signUp.html"
+                    })
+                    .state("signIn", {
+                        url: "/signIn",
+                        templateUrl: "./templates/signIn.html"
+                    })
+                    .state("tools", {
+                        url: "/tools",
+                        templateUrl: "./templates/tools.html"
+                    })
+                    .state("addTool", {
+                        url: "/addtool",
+                        templateUrl: "./templates/addTool.html"
+                    })
+                    .state("admin", {
+                        url: "/admin",
+                        templateUrl: "./templates/admin.html"
+                    });
+                $urlRouterProvider.otherwise("/");
+                $locationProvider.html5Mode(true);
+            }]);
+}());
